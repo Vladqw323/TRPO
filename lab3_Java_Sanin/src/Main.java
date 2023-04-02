4e34bf9d7fe24f01b632e30a96f67e8ce35ab2fb
vladqw <vlad.sanin.04@mail.ru>
2023-04-02 15:11:42 +0300

    lab3 commit

---
+++ lab3_Java_Sanin/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // Объявляем Scanner
+        System.out.println("Enter array length: ");
+        int size = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size
+        int array[] = new int[size]; // Создаём массив int размером в size
+        System.out.println("Insert array elements:");
+        /*Пройдёмся по всему массиву, заполняя его*/
+        for (int i = 0; i < size; i++) {
+            array[i] = input.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
+        }
+
+        System.out.println(Arrays.toString(array) + " --- inserted array elements");
+
+        sortByCount(array);
+        System.out.println(Arrays.toString(array) + " --- sorted array elements");
+    }
+
+    public static void sortByCount(int[] array) {
+
+        //Найдём максимальное число в массиве
+        int k = array[0]; //Максимальное число в массиве
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > k) {
+                k = array[i];
+            }
+        }
+
+        //Создадим новый массив длины k, по умолчанию наполненный нулями
+        int[] tempArray = new int[k + 1];
+
+        //Запишем в него количество вхождений каждого элемента поиндексно
+        for (int value : array) {
+            ++tempArray[value];
+        }
+
+        //Вставим элементы в исходный массив
+        int b = 0;
+        for (int i = 0; i < k + 1; ++i){
+            for (int j = 0; j < tempArray[i]; ++j) {
+                array[b++] = i;
+            }
+        }
+    }
+}
+
